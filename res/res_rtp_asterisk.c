f0efd956d92971994f96303d58b022787b8eae2c
Olle E. Johansson (Edvina) <oej@edvina.net>
2014-05-19 13:22:58 +0200

    Disabling earlier code to find out what is happening

--- res/res_rtp_asterisk.c
+++ res/res_rtp_asterisk.c
@@ -1882,6 +1882,7 @@ static void process_dtmf_rfc2833(struct ast_rtp_instance *instance, unsigned cha
 				} else {
 					ast_debug(4, "--GOT DTMF END message. Duration samples %d (%ld ms)\n", rtp->dtmf_duration, f->len);
 				}
+				ast_debug(4, "--GOT DTMF END message. Duration samples %d (%ld ms)\n", rtp->dtmf_duration, f->len);
 				rtp->resp = 0;
 				rtp->dtmf_duration = rtp->dtmf_timeout = 0;
 				AST_LIST_INSERT_TAIL(frames, f, frame_list);
