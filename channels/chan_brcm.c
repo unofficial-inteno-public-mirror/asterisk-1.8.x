96349cfa8723ae080b2a02f33739fce500686deb
Krister Jarl <kj@leissner.se>
2014-06-27 10:33:26 +0200

    Run endpoint driver configuration application on startup, refs #4210

--- channels/chan_brcm.c
+++ channels/chan_brcm.c
@@ -64,6 +64,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: 284597 $")
 #include "asterisk/indications.h"
 #include "asterisk/manager.h"
 #include "asterisk/sched.h"
+#include "asterisk/app.h"
 
 #include "chan_brcm.h"
 #include "chan_brcm_dect.h"
@@ -103,7 +104,6 @@ static int num_fxo_endpoints = -1;
 static int num_dect_endpoints = -1;
 static int num_endpoints = -1;
 static int endpoint_fd = NOT_INITIALIZED;
-static int endpoint_country = VRG_COUNTRY_NORTH_AMERICA;
 static int clip = 1; // Caller ID presentation
 static const format_t default_capability = AST_FORMAT_ALAW | AST_FORMAT_ULAW | AST_FORMAT_G729A | AST_FORMAT_G726; // AST_FORMAT_G723_1 breaks stuff
 struct ast_sched_thread *sched; //Scheduling thread
@@ -153,7 +153,6 @@ const DTMF_CHARNAME_MAP dtmf_to_charname[] =
 	{EPEVT_LAST,  "EPEVT_LAST", '-'}
 };
 
-
 static COUNTRY_MAP country_map[] =
 {
 	{VRG_COUNTRY_AUSTRALIA,			"AUS"},
@@ -184,6 +183,7 @@ static COUNTRY_MAP country_map[] =
 	{VRG_COUNTRY_CFG_TR57, 			"T57"}, //Not really an iso code
 	{VRG_COUNTRY_MAX, 			"-"}
 };
+static COUNTRY_MAP endpoint_country = {.vrgCountry = VRG_COUNTRY_NORTH_AMERICA, .isoCode = "USA"};
 
 /* Linked list of pvt:s */
 struct brcm_pvt *iflist;
@@ -2901,7 +2901,7 @@ static char *brcm_show_status(struct ast_cli_entry *e, int cmd, struct ast_cli_a
 	ast_cli(a->fd, "FXO  endpoints: %d\n", num_fxo_endpoints);
 	ast_cli(a->fd, "DECT endpoints: %d\n", num_dect_endpoints);
 	ast_cli(a->fd, "Endpoint fd   : 0x%x\n", endpoint_fd);
-	ast_cli(a->fd, "Country       : %d\n", endpoint_country);
+	ast_cli(a->fd, "Country       : %s\n", endpoint_country.isoCode);
 	ast_cli(a->fd, "Monitor thread: 0x%x[%d]\n", (unsigned int) monitor_thread, monitor);
 	ast_cli(a->fd, "Packet thread : 0x%x[%d]\n", (unsigned int) packet_thread, packets);
 
@@ -3614,7 +3614,7 @@ static int load_settings(struct ast_config **cfg)
 				ast_debug(2, "cmp: [%s] [%s]\n", v->value, countryMap->isoCode);
 				if (!strcmp(v->value, countryMap->isoCode)) {
 					ast_debug(2, "Found country '%s'\n", v->value);
-					endpoint_country = countryMap->vrgCountry; 
+					endpoint_country = *countryMap;
 					break;
 				}
 
@@ -3700,6 +3700,11 @@ static int load_module(void)
 
 	/* Initialize the endpoints */
 	endpt_init();
+
+	/* Set the provision data to the endpoint driver */
+	char config_cmd[32];
+	snprintf(config_cmd, 32, "endptcfg %s", endpoint_country.isoCode);
+	ast_safe_system(config_cmd);
 
 	brcm_get_endpoints_count();
 	load_endpoint_settings(cfg);
@@ -3877,7 +3882,7 @@ int endpt_init(void)
 
 	vrgEndptDriverOpen();
 
-	vrgEndptInitCfg.country = endpoint_country;
+	vrgEndptInitCfg.country = endpoint_country.vrgCountry;
 	vrgEndptInitCfg.currentPowerSource = 0;
 
 	/* Intialize endpoint */
@@ -3888,7 +3893,6 @@ int endpt_init(void)
 			   NULL,
 			   NULL,
 			   NULL );
-
 	
 	return 0;
 }
