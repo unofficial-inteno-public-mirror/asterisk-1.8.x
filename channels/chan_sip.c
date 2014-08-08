ae63e39f5db100a6fb5eca339a8fbbef14dc6ed1
Krister Jarl <kj@leissner.se>
2014-08-08 14:09:48 +0200

    sip: anonymous from-header bugfix, refs #5187

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -12181,9 +12181,10 @@ static void initreqprep(struct sip_request *req, struct sip_pvt *p, int sipmetho
 
 	snprintf(p->lastmsg, sizeof(p->lastmsg), "Init: %s", sip_methods[sipmethod].text);
 
+	int pres_allowed = (ast_party_id_presentation(&p->owner->connected.id) & AST_PRES_RESTRICTION) == AST_PRES_ALLOWED;
 	d = S_OR(p->fromdomain, ast_sockaddr_stringify_host_remote(&p->ourip));
 	if (p->owner) {
-		if ((ast_party_id_presentation(&p->owner->connected.id) & AST_PRES_RESTRICTION) == AST_PRES_ALLOWED) {
+		if (pres_allowed) {
 			l = p->owner->connected.id.number.valid ? p->owner->connected.id.number.str : NULL;
 			n = p->owner->connected.id.name.valid ? p->owner->connected.id.name.str : NULL;
 		} else {
@@ -12210,13 +12211,13 @@ static void initreqprep(struct sip_request *req, struct sip_pvt *p, int sipmetho
 	if (ast_strlen_zero(n))
 		n = l;
 	/* Allow user to be overridden */
-	if (!ast_strlen_zero(p->fromuser))
+	if (!ast_strlen_zero(p->fromuser) && pres_allowed)
 		l = p->fromuser;
 	else /* Save for any further attempts */
 		ast_string_field_set(p, fromuser, l);
 
 	/* Allow user to be overridden */
-	if (!ast_strlen_zero(p->fromname))
+	if (!ast_strlen_zero(p->fromname) && pres_allowed)
 		n = p->fromname;
 	else /* Save for any further attempts */
 		ast_string_field_set(p, fromname, n);
